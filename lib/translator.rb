08273dcbc62db1873f10fb14844f5362f8d653a2
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-14 21:02:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(emoticons)
   emoticons = YAML.load_file('lib/emoticons.yml')
   new_emoticons = { }
   emoticons.each do |word, face|
-    face.each do |value|
+    face.each do |key, value|
       new_emoticons[word]= {}
       new_emoticons[word][:english]=[]
       new_emoticons[word][:japanese]=[]
