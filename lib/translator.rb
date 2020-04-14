20a6abfac414f82d8b5be6a7bc43a57615517cad
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-14 20:30:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,7 +7,7 @@ def load_library(emoticons)
   emoticons.each do |key, value|
     if !new_emoticons[key]
       new_emoticons[key]= {}
-      new_emoticons[key] = {"#{value}"}
+      new_emoticons[key] = "#{value}"
     end
     binding.pry
   end
