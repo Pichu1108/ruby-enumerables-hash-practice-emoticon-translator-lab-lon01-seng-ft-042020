c1df88ceb3b8d498382a67fc518483a45258c4d6
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-14 21:57:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,6 +18,6 @@ end
 
 def get_english_meaning(file_path,emoticons)
 new_emoticons= load_library(file_path)
-  new_emoticons.has_value?(emoticons)
+  new_emoticons[word][:japanese].has_value(emoticons)
   binding.pry
 end
