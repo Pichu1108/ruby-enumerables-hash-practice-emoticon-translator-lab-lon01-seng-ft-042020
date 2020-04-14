5780ec6235dd8679fcaf541cf48ea87c1f17df31
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-14 22:19:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,7 @@ def get_japanese_emoticon(file_path,emoticon)
       return value[:japanese]
     end
     puts "Sorry, that emoticon was not found"
-
+  end
 end
 
 def get_english_meaning(file_path,emoticons)
