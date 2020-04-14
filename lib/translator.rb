af3d6854279b4e4ea0e489adc60f81507bf66205
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-14 19:51:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(emoticons)
   new_hash = {}
   emoticons.each do |word, face|
     new_hash[word] = {}
-    new_hash[word]<< :english,:japanese
+    new_hash[word]<< :english
   end
 
 
